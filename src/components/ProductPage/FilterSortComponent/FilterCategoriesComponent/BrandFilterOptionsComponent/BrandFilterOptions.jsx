e12b886ec16dbbbf45fdd3b9209d75104c0ceeac
Vaishnav V Nair <vaishnav8794@gmail.com>
2026-02-16 09:13:44 +0530

    Filter

--- src/components/ProductPage/FilterSortComponent/FilterCategoriesComponent/BrandFilterOptionsComponent/BrandFilterOptions.jsx
+++ src/components/ProductPage/FilterSortComponent/FilterCategoriesComponent/BrandFilterOptionsComponent/BrandFilterOptions.jsx
@@ -32,21 +32,14 @@ function BrandFilterOptions() {
           <input type="search" placeholder="Search Brand" />
         </div>
         <div className={styles.brandOptions}>
-          <CheckBox name="NIVIA" value="NIVIA" onChange={brandFilterValue} />
-          <CheckBox name="COSCO" value="COSCO" onChange={brandFilterValue} />
-          <CheckBox name="VECTORX" value="VECTORX" onChange={brandFilterValue} />
-          <CheckBox name="RASCO" value="RASCO" onChange={brandFilterValue} />
-          <CheckBox name="PUMA" value="PUMA" onChange={brandFilterValue} />
-          <CheckBox name="MAYOR" value="MAYOR" onChange={brandFilterValue} />
-          <CheckBox name="LatinX" value="LatinX" onChange={brandFilterValue} />
-          <CheckBox name="DENOVO" value="DENOVO" onChange={brandFilterValue} />
-          <CheckBox name="Dinetic" value="Dinetic" onChange={brandFilterValue} />
-          <CheckBox name="ULTIMATE GOAL" value="ULTIMATE GOAL" onChange={brandFilterValue} />
-          <CheckBox name="DIBACO SPORTS" value="DIBACO SPORTS" onChange={brandFilterValue} />
-          <CheckBox name="BOLDUP" value="BOLDUP" onChange={brandFilterValue} />
-          <CheckBox name="YMD" value="YMD" onChange={brandFilterValue} />
-          <CheckBox name="NICE" value="NINCE" onChange={brandFilterValue} />
-          <CheckBox name="gamers hub" value="gamers hub" onChange={brandFilterValue} />
+          <CheckBox name="RealMe" value="RealMe" onChange={brandFilterValue} />
+          <CheckBox name="IPHONE" value="IPhone" onChange={brandFilterValue} />
+          <CheckBox name="SAMSUNG" value="SAMSUNG" onChange={brandFilterValue} />
+          <CheckBox name="REDMI" value="REDMI" onChange={brandFilterValue} />
+          <CheckBox name="Ai+" value="Ai+" onChange={brandFilterValue} />
+          <CheckBox name="OPPO" value="OPPO" onChange={brandFilterValue} />
+          <CheckBox name="HOTLINK" value="HOTLINK" onChange={brandFilterValue} />
+          <CheckBox name="VIVO" value="VIVO" onChange={brandFilterValue} />
         </div>
       </div>
     </>
